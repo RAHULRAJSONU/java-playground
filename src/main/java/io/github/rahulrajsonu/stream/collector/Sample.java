82777e40cd80f2ac7012cad6933fb8c15413c8d3
RAHUL RAJ <RAJRAHUL9939@GMAIL.COM>
2025-04-17 20:08:06 +0530

    Collectors utility

--- src/main/java/io/github/rahulrajsonu/stream/collector/Sample.java
+++ src/main/java/io/github/rahulrajsonu/stream/collector/Sample.java
@@ -3,11 +3,16 @@ package io.github.rahulrajsonu.stream.collector;
 
 import io.github.rahulrajsonu.stream.Person;
 
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Sample {
     public static void main(String[] args) {
-        joinNamesInUpperCaseUsingComma();
+//        joinNamesInUpperCaseUsingComma();
+//        separateEvenAndOddAged();
+//        groupTheAgeByTheName();
+//        countTheName();
+        groupThePersonNameWithLengthGreaterThan2ByAge();
     }
 
     private static void joinNamesInUpperCaseUsingComma() {
@@ -15,5 +20,32 @@ public class Sample {
                 .map(Person::name)
                 .map(String::toUpperCase)
                 .collect(Collectors.joining(",")));
+    }
+
+    private static void separateEvenAndOddAged() {
+        System.out.println(Person.getPersonListDate().stream()
+                .collect(Collectors.partitioningBy(p->p.age() % 2==0)));
+    }
+
+    private static void groupTheAgeByTheName() {
+        System.out.println(Person.getPersonListDate().stream()
+                .collect(Collectors.groupingBy(Person::name,
+                        Collectors.mapping(Person::age, Collectors.toSet()))
+                )
+        );
+    }
+
+    private static void countTheName() {
+        System.out.println(Person.getPersonListDate().stream()
+                .collect(Collectors.groupingBy(Person::name,
+                        Collectors.collectingAndThen(Collectors.counting(), Long::intValue))));
+    }
+
+    private static void groupThePersonNameWithLengthGreaterThan2ByAge() {
+        System.out.println(Person.getPersonListDate().stream()
+                .collect(Collectors.groupingBy(Person::age,
+                        Collectors.mapping(Person::name,
+                                Collectors.filtering(n->n.length() > 4,
+                                        Collectors.toSet())))));
     }
 }
